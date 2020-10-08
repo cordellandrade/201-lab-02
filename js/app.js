5d5c548ea44068e5970deabbf9bf3dac4809b951
Cordell Andrade <cordellandrade93@gmail.com>
2020-10-08 00:32:14 -0500

    Fix js code

--- js/app.js
+++ js/app.js
@@ -45,41 +45,41 @@ var user = prompt('What is your name?');
 var  userPoints  =  0;
 
 
- function questionNumber1() {
-var response = prompt('Am I a student at Code Fellows?').toUpperCase();
+function questionNumber1() {
+    var response = prompt('Am I a student at Code Fellows?').toUpperCase();
 
-if (response === 'YES' || response === 'Y') {
-    // console.log('Correct! I am a student at Code Fellows')
-    alert('Correct! I am a student at Code Fellows');
-    userPoints++;
-}
+    if (response === 'YES' || response === 'Y') {
+        // console.log('Correct! I am a student at Code Fellows')
+        alert('Correct! I am a student at Code Fellows');
+        userPoints++;
+    }
 
 }
 questionNumber1();
 
 
 function questionNumber2() {
-var response = prompt('Do I live in the US?').toUpperCase();
+    var response = prompt('Do I live in the US?').toUpperCase();
 
-var message;
-if (response === 'NO' || response === 'N') {
-    // console.log('Wrong! I live in the Caribbean')
-    alert('Correct! I live in the Caribbean');
-    userPoints++;
-}
+    var message;
+    if (response === 'NO' || response === 'N') {
+        // console.log('Wrong! I live in the Caribbean')
+        alert('Correct! I live in the Caribbean');
+        userPoints++;
+    }
 }
 questionNumber2();
 
 
 function questionNumber3() {
-var response = prompt('Do I love dogs?').toUpperCase();
+    var response = prompt('Do I love dogs?').toUpperCase();
 
-var message;
-if (response === 'YES' || response === 'Y') {
-    // console.log('Correct!')
-    alert('Correct!');
-    userPoints++;
-}
+    var message;
+    if (response === 'YES' || response === 'Y') {
+        // console.log('Correct!')
+        alert('Correct!');
+        userPoints++;
+    }
 }
 
 questionNumber3();
@@ -87,28 +87,28 @@ questionNumber3();
 
 function questionNumber4() {
 
-var response = prompt('Did I live in the states before?').toUpperCase();
+    var response = prompt('Did I live in the states before?').toUpperCase();
 
-var message;
-if (response === 'YES' || response === 'Y') {
-    // console.log('Correct!')
-    alert('Correct!');
-    userPoints++;
-}
+    var message;
+    if (response === 'YES' || response === 'Y') {
+        // console.log('Correct!')
+        alert('Correct!');
+        userPoints++;
+    }
 }
 questionNumber4();
 
 
 function questionNumber5() {
 
-var response = prompt('Do I love sports?').toUpperCase();
+    var response = prompt('Do I love sports?').toUpperCase();
 
-var message;
-if (response === 'YES' || response === 'Y') {
-    // console.log('Correct!')
-    alert('Correct!');
-    userPoints++;
-}
+    var message;
+    if (response === 'YES' || response === 'Y') {
+        // console.log('Correct!')
+        alert('Correct!');
+        userPoints++;
+    }
 }
 questionNumber5();
 
@@ -127,12 +127,13 @@ questionNumber5(); */
 
 
 alert('Welcome to my guessing game!');
-function questionNumber6(){
+
+function questionNumber6() {
 
     var tries = 0;
     var guessNumber = 7; // correct answer
     var correctGuess = false;
-    
+
     while (tries < 3 && !correctGuess) {
         tries++;
         var guessGame = parseInt(prompt('I\'m thinking of a number from 1 - 10, can you guess the number?'));
@@ -146,7 +147,7 @@ function questionNumber6(){
             correctGuess = true;
         }
     }
-    
+
     if (!correctGuess) {
         alert(`
         You 're out of guesses, the correct one was  ${guessNumber}`);
@@ -160,14 +161,14 @@ questionNumber6();
 
 
 //7th Question
-function questionNumber7(){
+function questionNumber7() {
 
-    
+
     var whatMovie = ['the matrix', 'starwars', 'inception', 'the departer']
     var guessAnswer = prompt(' Can you guess what movie is my favorite?');
     var attemptsRemaining = 6;
     var answeredCorrect = false;
-    
+
     while (attemptsRemaining > 0 && !answeredCorrect) {
         attemptsRemaining--;
         for (var i = 0; i < whatMovie.length; i++) {
@@ -182,16 +183,16 @@ function questionNumber7(){
         if (attemptsRemaining === 0 && !answeredCorrect) {
             alert('Sorry you\'re out of tries!');
         }
+
+
     }
-}
-<<<<<<< HEAD
 
-if (!answeredCorrect) {
-    alert(`Thanks for playing, ${user}, the correct answer was the matrix :)`)
+    if (!answeredCorrect) {
+        alert(`Thanks for playing, ${user}, the correct answer was the matrix :)`)
+    }
+
+    alert('you have '  +  userPoints  +  ' points.');
+
 }
 
-=======
 questionNumber7();
-    
->>>>>>> 0a96120394790baa4e6f3f7b8d1ce5a42bec7e38
-alert('you have '  +  userPoints  +  ' points.');
